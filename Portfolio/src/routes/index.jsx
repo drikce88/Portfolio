b2e70ccc9f161bc9a06e2ad48b58e9abb268f0df
drikce88 <drikce88@gmail.com>
2024-05-23 14:51:35 +0200

    modif

--- Portfolio/src/routes/index.jsx
+++ Portfolio/src/routes/index.jsx
@@ -4,7 +4,7 @@ import Home from "../pages/Home";
 const index = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/Portfolio" element={<Home />} />
     </Routes>
   );
 };
