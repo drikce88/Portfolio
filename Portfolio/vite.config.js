5129277f4755bc2ba9c02e75d8733a90fe3e6f88
drikce88 <drikce88@gmail.com>
2024-05-23 16:10:35 +0200

    test config

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio',
-})
+  base: '/Portfolio/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx'
+      }
+    }
+  }
+});
